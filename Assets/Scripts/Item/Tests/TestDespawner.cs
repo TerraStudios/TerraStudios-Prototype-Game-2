3d4d1398222320a067b8ff672bb752a538a8690c
Konstantin Milev <milev109@gmail.com>
2020-06-14 18:49:43 +0300

    Implement TestSpawner and TestDespawner needed for benchmarks

---
+++ Assets/Scripts/Item/Tests/TestDespawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class TestDespawner : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer.Equals(11))
+            Destroy(other.gameObject);
+    }
+}
---
+++ Assets/Scripts/Item/Tests/TestSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (BoxCollider))]
+public class TestSpawner : MonoBehaviour
+{
+    public int spawnEverySeconds;
+    public ItemData itemToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoopSpawnItem());
+    }
+
+    IEnumerator LoopSpawnItem() 
+    {
+        yield return new WaitForSeconds(spawnEverySeconds);
+        Instantiate(itemToSpawn.obj, transform.position, Quaternion.identity);
+        StartCoroutine(LoopSpawnItem());
+    }
+}
