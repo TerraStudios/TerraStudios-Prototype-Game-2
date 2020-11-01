44c37dbf6c0bd00617ca1a07c92f92ed1be65133
Konstantin Milev <milev109@gmail.com>
2020-11-01 02:08:48 +0200

    [WIP] Save System 7th feature pass
    - Add save overwrite dialog and code
    - Add delete save button and code
    - Add save list to save panel
    - Give error when saves are duplicate and it's not overwriting
    - Add close buttons to close panels
    - Update TMPro to latest

---
+++ Assets/Scripts/Management/Save System/GameSaveBtn.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameSaveBtn : MonoBehaviour
+{
+    public TMP_Text SaveName;
+    public Button SaveSelectedBtn;
+    public Button TrashBtn;
+}
--- Assets/Scripts/Management/Save System/SaveManager.cs
+++ Assets/Scripts/Management/Save System/SaveManager.cs
@@ -7,19 +7,61 @@ using UnityEngine.UI;
 
 public class SaveManager : MonoBehaviour
 {
+    public static SaveManager instance;
+
     public Transform GameSaveButtonPrefab;
-    private List<GameObject> LoadedGameSaveButtons = new List<GameObject>();
-    public Transform GameSaveButtonsRoot;
+    public Transform SelectSavePanel;
+    public Transform SaveGameSavePanel;
+    public Transform ConfirmOverwritePanel;
+    public Button ConfirmOverwriteBtn;
+    private List<GameObject> LoadGameSaveButtons = new List<GameObject>();
+    private List<GameObject> SaveGameButtons = new List<GameObject>();
+    public Transform LoadSaveButtonsRoot;
+    public Transform SaveGameButtonsRoot;
     public TMP_InputField GameSaveName;
 
     public string[] saveFiles;
     public static bool saveLoaded;
 
+    private void Awake()
+    {
+        instance = this;
+    }
+
     public void ShowLoadScreen()
     {
         GetLoadFiles();
 
-        foreach(GameObject button in LoadedGameSaveButtons)
+        foreach(GameObject button in LoadGameSaveButtons)
+        {
+            Destroy(button);
+        }
+
+        for (int i = 0; i < saveFiles.Length; i++)
+        {
+            GameObject btnGO = Instantiate(GameSaveButtonPrefab, LoadSaveButtonsRoot).gameObject;
+            LoadGameSaveButtons.Add(btnGO);
+
+            GameSaveBtn btn = btnGO.GetComponent<GameSaveBtn>();
+
+            int index = i;
+            btn.SaveSelectedBtn.onClick.AddListener(() =>
+            {
+                OnSaveSelected(saveFiles[index]);
+            });
+            btn.TrashBtn.onClick.AddListener(() =>
+            {
+                OnGameSaveDelete(saveFiles[index]);
+            });
+            btn.SaveName.text = saveFiles[index].Replace(Application.persistentDataPath + "/saves/", "");
+        }
+    }
+
+    public void ShowSaveScreen() 
+    {
+        GetLoadFiles();
+
+        foreach (GameObject button in SaveGameButtons)
         {
             Destroy(button);
         }
@@ -28,16 +70,30 @@ public class SaveManager : MonoBehaviour
 
         for (int i = 0; i < saveFiles.Length; i++)
         {
-            GameObject btn = Instantiate(GameSaveButtonPrefab, GameSaveButtonsRoot).gameObject;
-            LoadedGameSaveButtons.Add(btn);
+            GameObject btnGO = Instantiate(GameSaveButtonPrefab, SaveGameButtonsRoot).gameObject;
+            SaveGameButtons.Add(btnGO);
+
+            GameSaveBtn btn = btnGO.GetComponent<GameSaveBtn>();
 
             int index = i;
-            btn.GetComponent<Button>().onClick.AddListener(() =>
+            btn.SaveSelectedBtn.onClick.AddListener(() =>
             {
-                OnSaveSelected(saveFiles[index]);
+                OnGameSaveSelectedOverwrite(saveFiles[index].Replace(Application.persistentDataPath + "/saves/", "").Replace(".pbag", ""));
             });
-            btn.GetComponentInChildren<TextMeshProUGUI>().text = saveFiles[index].Replace(Application.persistentDataPath + "/saves/", "");
+            btn.TrashBtn.onClick.AddListener(() =>
+            {
+                OnGameSaveDelete(saveFiles[index]);
+            });
+            btn.SaveName.text = saveFiles[index].Replace(Application.persistentDataPath + "/saves/", "");
         }
+    }
+
+    private void ReloadSavesList() 
+    {
+        if (SelectSavePanel)
+            ShowLoadScreen();
+        if (SaveGameSavePanel)
+            ShowSaveScreen();
     }
 
     public void OnSaveSelected(string saveName)
@@ -47,9 +103,43 @@ public class SaveManager : MonoBehaviour
         GameManager.instance.ResetGame();
     }
 
-    public void OnSaveGame()
+    public void OnGameSaveSelectedOverwrite(string saveName)
     {
+        ConfirmOverwritePanel.gameObject.SetActive(true);
+        SaveGameSavePanel.gameObject.SetActive(false);
+        ConfirmOverwriteBtn.onClick.RemoveAllListeners();
+        ConfirmOverwriteBtn.onClick.AddListener( () => OnConfimOverwirte(saveName) );
+    }
+
+    private void OnConfimOverwirte(string saveName) 
+    {
+        ConfirmOverwritePanel.gameObject.SetActive(false);
+        SaveGameSavePanel.gameObject.SetActive(true);
+        SerializationManager.Save(saveName, GameSave.current);
+    }
+
+    public void OnGameSaveDelete(string saveName) 
+    {
+        File.Delete(saveName);
+        ReloadSavesList();
+    }
+
+    public void OnSaveGameManualBtn()
+    {
+        if (File.Exists(Application.persistentDataPath + "/saves/" + GameSaveName.text + ".pbag"))
+        {
+            Debug.LogError("Save already exists");
+            return;
+        }
+
         SerializationManager.Save(GameSaveName.text, GameSave.current);
+        ReloadSavesList();
+        GameSaveName.text = "";
+    }
+
+    public void SaveAlreadyExistsDialog() 
+    {
+        
     }
 
     public void GetLoadFiles()
