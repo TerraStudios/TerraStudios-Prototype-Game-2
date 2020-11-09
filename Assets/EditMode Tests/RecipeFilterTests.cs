fbfd9eae1731bb65e03f23ca95bb418f6f2f3b4c
Konstantin Milev <milev109@gmail.com>
2020-11-09 22:58:40 +0200

    Add "possibly invalid recipe" test

---
+++ Assets/EditMode Tests/RecipeFilterTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Assets.Tests
+{
+    public class RecipeFilterTests
+    {
+        [Test]
+        public void CheckCorrectRecipeFilterAttachedToAPM() 
+        {
+            foreach (RecipeFilter r in Resources.LoadAll<RecipeFilter>(""))
+            {
+                if (r.enableAutomaticList && r.inputsAmount == 0 && r.type == RecipeType.Allowed)
+                    Assert.IsFalse(r.enableAutomaticList && r.inputsAmount == 0 && r.type == RecipeType.Allowed, $"Possibly invalid recipe filter detected: {r.name}");
+            }
+        }
+    }
+}
