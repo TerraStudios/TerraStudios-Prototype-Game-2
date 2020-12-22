dee5e16ec18805d27c31d263cd3d5a392afbfb74
Yerti <36551149+UZ9@users.noreply.github.com>
2020-12-22 17:58:09 -0600

    Moved terrain generation to TerrainGeneration namespace

--- Assets/Scripts/Terrain/Generator/VoxelTables.cs
+++ Assets/Scripts/Terrain/Generator/VoxelTables.cs
@@ -1,45 +1,47 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// Stores constant data about each voxel (cube)
-/// </summary>
-static class VoxelTables
+namespace TerrainGeneration
 {
 	/// <summary>
-	/// Size in pixels of a block in the sprite map
+	/// Stores constant data about each voxel (cube)
 	/// </summary>
-	public static readonly Vector2 BlockSize = new Vector2(128, 128);
-	
-	/// <summary>
-	/// Amount of blocks in length of the sprite
-	/// </summary>
-	public static readonly int TextureAtlasScaleX = (int) (TerrainGenerator.material.mainTexture.width / BlockSize.x);
-	
-	/// <summary>
-	/// Amount of blocks in width of the sprite
-	/// </summary>
-	public static readonly int TextureAtlasScaleY = (int) (TerrainGenerator.material.mainTexture.height / BlockSize.y);
-	
-	/// <summary>
-	/// Normalizes the x texture size, used for calculating UVs
-	/// </summary>
-	public static float NormalizedTextureSizeX
-    {
-		get { return 1f / TextureAtlasScaleX; }
-	}
+	static class VoxelTables
+	{
+		/// <summary>
+		/// Size in pixels of a block in the sprite map
+		/// </summary>
+		public static readonly Vector2 BlockSize = new Vector2(128, 128);
 
-	/// <summary>
-	/// Normalizes the y texture size, used for calculating UVs
-	/// </summary>
-	public static float NormalizedTextureSizeY
-    {
-		get { return 1f / (float)TextureAtlasScaleY; }
-    }
+		/// <summary>
+		/// Amount of blocks in length of the sprite
+		/// </summary>
+		public static readonly int TextureAtlasScaleX = (int)(TerrainGenerator.material.mainTexture.width / BlockSize.x);
 
-	/// <summary>
-	/// Lookup table for the vertices in a cube
-	/// </summary>
-	public static readonly Vector3[] voxelVerts = new Vector3[8] {
+		/// <summary>
+		/// Amount of blocks in width of the sprite
+		/// </summary>
+		public static readonly int TextureAtlasScaleY = (int)(TerrainGenerator.material.mainTexture.height / BlockSize.y);
+
+		/// <summary>
+		/// Normalizes the x texture size, used for calculating UVs
+		/// </summary>
+		public static float NormalizedTextureSizeX
+		{
+			get { return 1f / TextureAtlasScaleX; }
+		}
+
+		/// <summary>
+		/// Normalizes the y texture size, used for calculating UVs
+		/// </summary>
+		public static float NormalizedTextureSizeY
+		{
+			get { return 1f / (float)TextureAtlasScaleY; }
+		}
+
+		/// <summary>
+		/// Lookup table for the vertices in a cube
+		/// </summary>
+		public static readonly Vector3[] voxelVerts = new Vector3[8] {
 
 		new Vector3(0.0f, 0.0f, 0.0f),
 		new Vector3(1.0f, 0.0f, 0.0f),
@@ -52,23 +54,23 @@ static class VoxelTables
 
 	};
 
-	/// <summary>
-	/// Lookup table for the faces of a cube
-	/// </summary>
-	public static readonly Vector3Int[] faces = new Vector3Int[6]
-	{
+		/// <summary>
+		/// Lookup table for the faces of a cube
+		/// </summary>
+		public static readonly Vector3Int[] faces = new Vector3Int[6]
+		{
 		new Vector3Int(0, 0, -1),
 		new Vector3Int(0, 0, 1),
 		new Vector3Int(0, 1, 0),
 		new Vector3Int(0, -1, 0),
 		new Vector3Int(-1, 0, 0),
 		new Vector3Int(1, 0, 0)
-	};
+		};
 
-	/// <summary>
-	/// Lookup table for the triangles of a cube
-	/// </summary>
-	public static readonly int[,] voxelTris = new int[6, 4] {
+		/// <summary>
+		/// Lookup table for the triangles of a cube
+		/// </summary>
+		public static readonly int[,] voxelTris = new int[6, 4] {
 
 		{0, 3, 1, 2}, // Back Face
 		{5, 6, 4, 7}, // Front Face
@@ -79,10 +81,10 @@ static class VoxelTables
 
 	};
 
-	/// <summary>
-	/// Lookup table for the UVs of a cube
-	/// </summary>
-	public static readonly Vector2[] voxelUvs = new Vector2[4] {
+		/// <summary>
+		/// Lookup table for the UVs of a cube
+		/// </summary>
+		public static readonly Vector2[] voxelUvs = new Vector2[4] {
 
 		new Vector2 (0.0f, 0.0f),
 		new Vector2 (0.0f, 1.0f),
@@ -90,4 +92,5 @@ static class VoxelTables
 		new Vector2 (1.0f, 1.0f)
 
 	};
+	}
 }
