ed9a73a69a8972c1c345f1a2bb67dc63e323a53e
Konstantin Milev <milev109@gmail.com>
2021-08-17 02:48:19 +0300

    Change the header of all scripts

--- Assets/Scripts/InputSystem/MovementVector3Composite.cs
+++ Assets/Scripts/InputSystem/MovementVector3Composite.cs
@@ -1,7 +1,12 @@
 ﻿//
-// Developed by TerraStudios.
-// This script is covered by a Mutual Non-Disclosure Agreement and is Confidential.
-// Destroy the file immediately if you are not one of the parties involved.
+// Developed by TerraStudios (https://github.com/TerraStudios)
+//
+// Copyright(c) 2020-2021 Konstantin Milev (konstantin890 | milev109@gmail.com)
+// Copyright(c) 2020-2021 Yerti (UZ9)
+//
+// The following script has been written by either konstantin890 or Yerti (UZ9) or both.
+// This file is covered by the GNU GPL v3 license. Read LICENSE.md for more information.
+// Past NDA/MNDA and Confidential notices are revoked and invalid since no agreement took place. Read README.md for more information.
 //
 
 using UnityEditor;
